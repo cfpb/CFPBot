c045af755214e1f13819b9dab55e474b4a14dcee
Chris Contolini <contolini@users.noreply.github.com>
2016-04-21 16:59:33 -0400

    :sweat_drops

--- start.sh
+++ start.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
-source ./.env
-npm run load
+ENV_FILE="./.env"
+ENV_PRIVATE_FILE="./.env_private"
+
+if [ -f "$ENV_FILE" ]; then
+   source $ENV_FILE
+fi
+
+if [ -n "$HUBOT_ENV_LOCATION" ]; then
+  curl -O $HUBOT_ENV_LOCATION
+  source $ENV_FILE
+fi
+
+if [ -f "$ENV_PRIVATE_FILE" ]; then
+   source $ENV_PRIVATE_FILE
+fi
+
+./bin/hubot --adapter matteruser --alias $MATTERMOST_HUBOT_USERNAME,.,please,bot,uhh,uhhh
